107299526aa04f7c3c7c7b40c0df3dff2e393f66
hbadhe13 <hbadhe13@gmail.com>
2017-07-26 20:27:02 +0530

    Add files via upload

---
+++ Mall/BillingCounter.java
@@ -0,0 +1,16 @@
+package Mall;
+
+public class BillingCounter {
+
+    public static void main(String[] args) {
+
+        Item[] items = new Item[2];
+        Cloth cloth = new Cloth();
+        Toy toy = new Toy();
+        cloth.buyItem("Shirt",500,2);
+        toy.buyItem("Teddy Bear",400,2);
+        items[0] = cloth;
+        items[1] = toy;
+        new Invoice().print(items);
+    }
+}
---
+++ Mall/Item.java
@@ -0,0 +1,20 @@
+package Mall;
+
+public abstract class Item {
+    String itemName = "";
+    float price = 0.0F;
+    int quantity = 0;
+    float gstValue = 0.0F;
+
+    public Item() {
+    }
+
+    abstract float getGST(float var1, int var2);
+
+    void setItem(String var1, float var2, int var3, float var4) {
+        this.itemName = var1;
+        this.price = var2;
+        this.quantity = var3;
+        this.gstValue = var4;
+    }
+}
